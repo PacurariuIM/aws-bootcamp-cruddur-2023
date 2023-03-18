a97633bc43a3aa0c5490e48940f2df16426db32a
PacurariuIM <jonny_boy90609@yahoo.com>
2023-03-18 13:13:55 +0000

    9th w04(finally working)

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,7 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id, email)
 VALUES
-  ('Ionel Pacurariu', 'ionelp' ,'e2d787ae-5a98-4d02-890e-7231a9a06d2d', 'jonny_boy90609@yahoo.com'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Ionel Pacurariu', 'ionelp' ,'e2d787ae-5a98-4d02-890e-7231a9a06d2d', 'jonny_boy90609@yahoo.com');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
