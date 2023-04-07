a40e1a0d434f4c2e03fa7175e6e38070abbe101a
Ionel Pacurariu <ionel.crazyfroggg@gmail.com>
2023-04-07 18:08:31 +0000

    8th w06 (refactoring bin, env)

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -125,11 +125,11 @@ def init_rollbar():
 @app.route('/api/health-check')
 def health_check():
   return {'success': True}, 200
-  
-# @app.route('/rollbar/test')
-# def rollbar_test():
-#     rollbar.report_message('Hello World!', 'warning')
-#     return "Hello World!"
+
+#@app.route('/rollbar/test')
+#def rollbar_test():
+#    rollbar.report_message('Hello World!', 'warning')
+#    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Ionel Pacurariu', 'jonny_boy90609@yahoo.com', 'ionelp' ,'e2d787ae-5a98-4d02-890e-7231a9a06d2d'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Londo Mollari', 'lmollari@centari.com','londo','MOCK');
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
