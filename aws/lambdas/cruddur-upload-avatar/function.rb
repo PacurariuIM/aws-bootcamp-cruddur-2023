743c296fb5243cc421f397b42100f0cf6f39805b
PacurariuIM <jonny_boy90609@yahoo.com>
2023-05-08 09:21:29 +0000

    lambda small update

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -10,7 +10,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://https://3000-pacurariuim-awsbootcamp-ckvvlqgjsc3.ws-eu96b.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-pacurariuim-awsbootcamp-io161wgiid4.ws-eu96b.gitpod.io/",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200
@@ -38,7 +38,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://https://3000-pacurariuim-awsbootcamp-ckvvlqgjsc3.ws-eu96b.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-pacurariuim-awsbootcamp-io161wgiid4.ws-eu96b.gitpod.io/",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200, 
