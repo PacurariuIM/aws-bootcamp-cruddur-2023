50bd79a3725eadd856d8745d6f97261ea3c93930
PacurariuIM <jonny_boy90609@yahoo.com>
2023-05-09 14:49:13 +0000

    update health checl 3

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -126,7 +126,7 @@ with app.app_context():
 
 @app.route('/api/health-check')
 def health_check():
-  return {'success': True, 'ver' : 1.1}, 200
+  return {'success': True, 'ver' : 1.2}, 200
 
 #@app.route('/rollbar/test')
 #def rollbar_test():
