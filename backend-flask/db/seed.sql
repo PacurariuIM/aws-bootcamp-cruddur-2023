48126b838726f5996d208c79ff4ff6a77223970b
PacurariuIM <jonny_boy90609@yahoo.com>
2023-07-12 04:37:58 +0000

    12th wX (activity show page, p2)

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -11,5 +11,10 @@ VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'cheloo' LIMIT 1),
     'This was imported as seed data!',
+    current_timestamp 0 interval '10 day'
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'crazyfroggg' LIMIT 1),
+    'I am the other!',
     current_timestamp + interval '10 day'
-  )
+  );
