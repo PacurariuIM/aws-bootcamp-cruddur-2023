ae2dc3d29dc39b38d4313ea4b071dd912ee6beba
PacurariuIM <jonny_boy90609@yahoo.com>
2023-08-10 16:30:18 +0000

    small fix

--- frontend-react-js/src/components/ActivityShowItem.js
+++ frontend-react-js/src/components/ActivityShowItem.js
@@ -15,41 +15,36 @@ export default function ActivityShowItem(props) {
   attrs.className = 'activity_item expanded'
   return (
     <div {...attrs}>
-      <div className="acitivty_main">
-        <div className='activity_content_wrap'>
-          <div className='activity_content'>
-            <Link className='activity_avatar'to={`/@`+props.activity.handle} ></Link>
-            <div className='activity_meta'>
-              <div className='activity_identity' >
-                <Link className='display_name' to={`/@`+props.activity.handle}>{props.activity.display_name}</Link>
-                <Link className="handle" to={`/@`+props.activity.handle}>@{props.activity.handle}</Link>
-              </div>{/* activity_identity */}
-              <div className='activity_times'>
-                <div className="created_at" title={format_datetime(props.activity.created_at)}>
-                  <span className='ago'>{time_ago(props.activity.created_at)}</span> 
-                </div>
-                <div className="expires_at" title={format_datetime(props.activity.expires_at)}>
+    <div className="activity_main">
+      <div className='activity_content_wrap'>
+        <Link className='activity_avatar'to={`/@`+props.activity.handle} ></Link>
+        <div className='activity_content'>
+          <div className='activity_meta'>
+            <div className='activity_identity' >
+              <Link className='display_name' to={`/@`+props.activity.handle}>{props.activity.display_name}</Link>
+              <Link className="handle" to={`/@`+props.activity.handle}>@{props.activity.handle}</Link>
+            </div>{/* activity_identity */}
+            <div className='activity_times'>
+              <div className="created_at" title={format_datetime(props.activity.created_at)}>
+                <span className='ago'>{time_ago(props.activity.created_at)}</span>
+              </div>
+              <div className="expires_at" title={format_datetime(props.activity.expires_at)}>
                   <BombIcon className='icon' />
                   <span className='ago'>{time_future(props.activity.expires_at)}</span>
                 </div>
-              </div>{/* activity_times */}
-            </div>{/* activity_meta */}
-          </div>{/* activity_content */}
+            </div>{/* activity_times */}
+          </div>{/* activity_meta */}
           <div className="message">{props.activity.message}</div>
-        </div>
+        </div>{/* activity_content */}
+      </div>
 
-        <div className='expandedMeta'>
-          <div class="created_at">
-            {format_datetime(props.activity.created_at)}
-          </div>
-        </div>
-        <div className="activity_actions">
-          <ActivityActionReply setReplyActivity={props.setReplyActivity} activity={props.activity} setPopped={props.setPopped} activity_uuid={props.activity.uuid} count={props.activity.replies_count}/>
-          <ActivityActionRepost activity_uuid={props.activity.uuid} count={props.activity.reposts_count}/>
-          <ActivityActionLike activity_uuid={props.activity.uuid} count={props.activity.likes_count}/>
-          <ActivityActionShare activity_uuid={props.activity.uuid} />
-        </div>
+      <div className="activity_actions">
+        <ActivityActionReply setReplyActivity={props.setReplyActivity} activity={props.activity} setPopped={props.setPopped} activity_uuid={props.activity.uuid} count={props.activity.replies_count}/>
+        <ActivityActionRepost activity_uuid={props.activity.uuid} count={props.activity.reposts_count}/>
+        <ActivityActionLike activity_uuid={props.activity.uuid} count={props.activity.likes_count}/>
+        <ActivityActionShare activity_uuid={props.activity.uuid} />
       </div>
     </div>
+  </div>
   )
 }
